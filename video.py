4e5c387d6e991a55ff3cf177866e47f0e466f7c2
Noway-code <dragonfury1928@gmail.com>
2026-03-05 13:21:37 -0500

    Move script to seperate file

--- video.py
+++ video.py
@@ -1,21 +1,62 @@
-# import the necessary packages
-from imutils.video import VideoStream
-import argparse
-import datetime
-import imutils
+from picamera2 import MappedArray, Picamera2
+import cv2
+import numpy as np
 import time
-import cv1
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", help="path to the video file")
-ap.add_argument("-a", "--min-area", type=int, default=499, help="minimum area size")
-args = vars(ap.parse_args())
-# if the video argument is None, then we are reading from webcam
-if args.get("video", None) is None:
-    vs = VideoStream(src=-1).start()
-    time.sleep(1.0)
-# otherwise, we are reading from a video file
-else:
-    vs = cv1.VideoCapture(args["video"])
-# initialize the first frame in the video stream
-firstFrame = None
+from time import perf_counter
+import matplotlib.pyplot as plt
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import PyavOutput
+
+# 1. Initialize the camera object
+picam2 = Picamera2()
+
+config = picam2.create_video_configuration(
+    main={"size": (1280, 720), "format": "RGB888"}
+)
+picam2.configure(config)
+
+colour = (0, 255, 0)
+origin = (0, 30)
+font = cv2.FONT_HERSHEY_SIMPLEX
+scale = 1
+thickness = 2
+
+def apply_timestamp(request):
+    timestamp = time.strftime("%Y-%m-%d %X")
+    with MappedArray(request, "main") as m:
+        cv2.putText(m.array, timestamp, origin, font, scale, colour, thickness)
+        
+picam2.pre_callback = apply_timestamp
+picam2.start()
+
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+out = cv2.VideoWriter("out/output.mp4", fourcc, 20, (1280, 720))
+
+# 4. Loop to capture and display frame
+start = perf_counter()
+frame1 = picam2.capture_array()
+frame2 = picam2.capture_array()
+while (perf_counter() - start ) < 15:
+    frame_display = frame2.copy()
+    # Capture a frame as a NumPy array
+    diff = cv2.absdiff(frame1, frame2)
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0) 
+    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    
+    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in contours:
+        if cv2.contourArea(c) > 900:
+            print("Movement detected")
+            # get the bounding box coordinates
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(frame_display, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    out.write(frame_display)
+    frame1 = frame2
+    frame2 = picam2.capture_array()
+
+out.release()
+picam2.stop()
+picam2.close()
