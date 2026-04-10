2e1a433b256a0f5b9d8e30ac9722bc76374dd3c4
Noway-code <dragonfury1928@gmail.com>
2026-04-09 22:00:14 -0400

    fix minor conversion bug

--- src/video.py
+++ src/video.py
@@ -6,16 +6,26 @@ from time import perf_counter
 import matplotlib.pyplot as plt
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import PyavOutput
+import requests
+import os
+import traceback
 
-def apply_timestamp(request):
-    timestamp = time.strftime("%Y-%m-%d %X")
-    with MappedArray(request, "main") as m:
-        cv2.putText(m.array, timestamp, (0,30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1,
-                    (0,255,0), 2)
+CLIP_DURATION = 20
+MOTION_AREA_THRESHOLD = 900
+OUTPUT_DIR = "out"
+ENDPOINT = "http://localhost:8000/uploadfile/"
+
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+recording = False
+record_start = None
+writer = None
+
 
 class Camera:
-    def __init__(self, main_size: tuple, main_format: str, lores_size: tuple, lores_format: str):
+    def __init__(
+        self, main_size: tuple, main_format: str, lores_size: tuple, lores_format: str
+    ):
         self.picam2 = Picamera2()
         self.main_size = main_size
         self.main_format = main_format
@@ -28,7 +38,7 @@ class Camera:
         # Configure main and lores
         self.config = self.picam2.create_video_configuration(
             main={"size": self.main_size, "format": self.main_format},
-            lores={"size": self.lores_size, "format": self.lores_format}
+            lores={"size": self.lores_size, "format": self.lores_format},
         )
         self.picam2.configure(self.config)
 
@@ -40,8 +50,8 @@ class Camera:
 
     def capture_array(self, name="main"):
         return self.picam2.capture_array(name=name)
-    
-    def start_encoder(self, name='main'):
+
+    def start_encoder(self, name="main"):
         return self.picam2.start_encoder
 
     def stop(self):
@@ -52,44 +62,91 @@ class Camera:
 
 
 # Initiatlize and Config the Camera
-main_size=(1280,720)
-main_format="RGB888"
-lores_size=(640,360)
-lores_format="YUV420"
+main_size = (1280, 720)
+main_format = "RGB888"
+lores_size = (640, 360)
+lores_format = "YUV420"
+threshold = 900
 
 camera = Camera(main_size, main_format, lores_size, lores_format)
-
-# Immediatly on frame capture, add timestamp
-camera.set_pre_callback = apply_timestamp
 camera.start()
 
-# Prepare to output video
-fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-out = cv2.VideoWriter("out/output.mp4", fourcc, 20, (1280, 720))
-
-start = perf_counter()
-
 frame1 = camera.capture_array("lores")
-frame1 = frame1[:lores_size[1], :]   # Y channel only
+frame1 = frame1[: lores_size[1], :]  # Y channel only
+filepath = None
+try:
+    while True:
+        frame2 = camera.capture_array("lores")
+        frame2 = frame2[: lores_size[1], :]
 
-while (perf_counter() - start) < 15:
-    frame2 = camera.capture_array("lores")
-    frame2 = frame2[:lores_size[1], :]
+        diff = cv2.absdiff(frame1, frame2)
+        blur = cv2.GaussianBlur(diff, (5, 5), 0)
 
-    diff = cv2.absdiff(frame1, frame2)
-    blur = cv2.GaussianBlur(diff, (5,5), 0)
+        _, thresh = cv2.threshold(blur, 25, 255, cv2.THRESH_BINARY)
+        thresh = cv2.dilate(thresh, None, iterations=2)
 
-    _, thresh = cv2.threshold(blur, 25, 255, cv2.THRESH_BINARY)
-    thresh = cv2.dilate(thresh, None, iterations=2)
+        contours, _ = cv2.findContours(
+            thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        )
 
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # Extremly sensitive
+        motion_detected = any(
+            cv2.contourArea(c) > MOTION_AREA_THRESHOLD for c in contours
+        )
 
-    for c in contours:
-        if cv2.contourArea(c) > 900:
-            print("motion")
-    out.write(cv2.cvtColor(frame2, cv2.COLOR_YUV420p2RGB))
-    frame1 = frame2
+        if motion_detected and not recording:
+            print("motion detected -> start recording")
 
-out.release()
-camera.stop()
-camera.close()
+            recording = True
+            record_start = perf_counter()
+
+            filename = time.strftime("%Y%m%d_%H%M%S") + ".mp4"
+            # store it to out/<currenttime>.mp4
+            filepath = os.path.join(OUTPUT_DIR, filename)
+
+            # Setup writer for this clip and will terminate afterwards
+            writer = cv2.VideoWriter(
+                filepath,
+                cv2.VideoWriter_fourcc(*"mp4v"),  # Fourcc - ASCII code for format
+                20,  # FPS
+                (main_size[0], main_size[1]),  # Width/Height
+            )
+
+        # RECORDING IN PROGRESS
+        if recording:
+            if record_start is None or writer is None or filepath is None:
+                break
+            frame_main = camera.capture_array("main")
+            print(frame_main.shape, frame_main.dtype)
+            frame_main = cv2.cvtColor(frame_main, cv2.COLOR_RGB2BGR)
+            print(frame_main.shape, frame_main.dtype)
+            writer.write(frame_main)
+
+            # If video max time is reached
+            if (perf_counter() - record_start) >= CLIP_DURATION:
+                print("recording finished")
+
+                writer.release()
+                recording = False
+
+                # SEND FILE
+                try:
+                    with open(filepath, "rb") as f:
+                        response = requests.post(
+                            ENDPOINT,
+                            data=f,
+                            headers={"Content-Type": "video/mp4"},
+                            stream=True,
+                        )
+                        print("upload status:", response.status_code)
+                except Exception as e:
+                    print("upload failed:", e)
+        frame1 = frame2
+except Exception as e:
+    traceback.print_exc()
+finally:
+    if recording is True and writer is not None:
+        writer.release()
+        recording = False
+    camera.stop()
+    camera.close()
